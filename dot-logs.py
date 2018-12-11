da03cbf9c072553b1e35f4c27e2d09b8933637db
Heiko Schocher <hs@denx.de>
2018-12-11 13:05:40 +0100

    generators: dot
    Signed-off-by: Heiko Schocher <hs@denx.de>

---
+++ dot-logs.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+import typing
+import pathlib
+import subprocess
+import os
+
+CONFS = [
+    ("raspi", "sanvito"),
+]
+
+def do_conf(cfg: typing.Tuple[str, str, str]) -> None:
+    # Find latest config
+    logs = list(pathlib.Path("log").glob(f"{cfg[0]}-{cfg[1]}-*.json"))
+    logs.sort()
+    log = logs[-1]
+    dotdir = pathlib.Path("results/dot")
+    dotdir.mkdir(exist_ok=True)
+    dot_name = dotdir / f"{log.stem}.dot"
+    png_name = dotdir / f"{log.stem}.png"
+    title = f"{log.stem}"
+    print(f"{log} -> {dot_name}")
+    handle = subprocess.Popen(
+        ["generators/dot.py", log], stdout=subprocess.PIPE
+    )
+    stats = handle.stdout.read().decode("utf-8")
+    with open(dot_name, mode="w") as f:
+        f.write(stats)
+
+    cmd = "dot -Tpng " + str(dot_name) + " > "  + str(png_name)
+    os.system(cmd)
+
+def main() -> None:
+    for conf in CONFS:
+        do_conf(conf)
+
+if __name__ == "__main__":
+    main()
