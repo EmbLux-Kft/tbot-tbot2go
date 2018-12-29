f23c040d2ea6b7bb922af2ea4216a1d27086aaa6
Heiko Schocher <hs@denx.de>
2018-12-29 14:03:28 +0100

    boards/raspi.py: add k30rf 16mb board
    use flag "16mb" for 16 mb version of k30rf board.
    
    Signed-off-by: Heiko Schocher <hs@denx.de>

--- boards/raspi.py
+++ boards/raspi.py
@@ -1,3 +1,4 @@
+import tbot
 from tbot.machine import channel
 from tbot.machine import board
 import os,sys,inspect
@@ -15,23 +16,30 @@ class Tbot2goUBootBuildInfo(uboot.BuildInfo):
 
 class Tbot2goBoard(board.Board):
     connect_wait = 1.0
+    pin = "3"
+    boardlabname = "k30rf"
+    if "16mb" in tbot.flags:
+        pin = "4"
+        boardlabname = "k30rf-16mb"
 
     def poweron(self) -> None:
         if self.name == 'k30rf':
-            self.lh.exec0("power.py", "-p", "19", "-s", "on")
+            #self.lh.exec0("power.py", "-p", "19", "-s", "on")
+            self.lh.exec0("sudo", "/work/tbot2go/tbot/src/pyrelayctl/examples/relctl.py","-D", "A907QJT3", "-o", self.pin)
         else:
             raise RuntimeError("Board ", self.name, " not configured")
 
     def poweroff(self) -> None:
         if self.name == 'k30rf':
-            self.lh.exec0("power.py", "-p", "19", "-s", "off")
+            #self.lh.exec0("power.py", "-p", "19", "-s", "off")
+            self.lh.exec0("sudo", "/work/tbot2go/tbot/src/pyrelayctl/examples/relctl.py","-D", "A907QJT3", "-f", self.pin)
         else:
             raise RuntimeError("Board ", self.name, " not configured")
 
     def connect(self) -> channel.Channel:
         KERMIT_PROMPT = b"C-Kermit>"
         if self.name == 'k30rf':
-            cfg_file = "/home/pi/kermrc_k30rf"
+            cfg_file = "/home/pi/kermrc_" + self.boardlabname
         else:
             raise RuntimeError("Board ", self.name, " console not configured")
         ch = self.lh.new_channel("kermit", cfg_file)
