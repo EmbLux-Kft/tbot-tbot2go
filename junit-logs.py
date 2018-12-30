97ecb62e476ff82b88356a2ab33ba573e422eff2
Heiko Schocher <hs@denx.de>
2018-12-30 14:42:29 +0100

    generator: add junit generator
    Signed-off-by: Heiko Schocher <hs@denx.de>

---
+++ junit-logs.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+import typing
+import pathlib
+import subprocess
+
+CONFS = [
+    ("raspi", "piinstall"),
+]
+
+
+def do_conf(cfg: typing.Tuple[str, str]) -> None:
+    # Find latest config
+    logs = list(pathlib.Path("log").glob(f"{cfg[0]}-{cfg[1]}-*.json"))
+    logs.sort()
+    log = logs[-1]
+    junitdir = pathlib.Path("results/junit")
+    junitdir.mkdir(exist_ok=True)
+    junit_name = junitdir / f"{log.stem}.xml"
+    print(f"{log} -> {junit_name}")
+    handle = subprocess.Popen(
+        ["../tbot/generators/junit.py", log], stdout=subprocess.PIPE
+    )
+    junit = handle.stdout.read().decode("utf-8")
+    with open(junit_name, mode="w") as f:
+        f.write(junit)
+
+
+def main() -> None:
+    for conf in CONFS:
+        do_conf(conf)
+
+if __name__ == "__main__":
+    main()
