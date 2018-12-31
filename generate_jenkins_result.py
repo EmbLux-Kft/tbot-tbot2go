2336a555580862e3b2e189f21bbd1fb6e5ae5f42
Heiko Schocher <hs@denx.de>
2018-12-31 10:40:17 +0100

    generate_jenkins_result.py: use results/html/myscript.js
    use results/html/myscript.js
    
    Signed-off-by: Heiko Schocher <hs@denx.de>

--- generate_jenkins_result.py
+++ generate_jenkins_result.py
@@ -11,16 +11,19 @@ if len(sys.argv) < 2:
 
 jenkins_workspace = sys.argv[1]
 
+# generate junit file
 res = subprocess.run(["./junit-logs.py"], stdout=subprocess.PIPE)
 result = str(res.stdout.decode('utf-8'))
 fn = result.split("->")
 fn = fn[1].strip()
 res = subprocess.run(["cp", fn, jenkins_workspace + "/tbot_results.xml"], stdout=subprocess.PIPE)
 
+# setup subdir for tbot results
 classname = "00 - piinstall_install_tools.00000 - piinstall_install_tools"
 jenkins_workspace_tbot = jenkins_workspace + "/" + classname
 res = subprocess.run(["mkdir", jenkins_workspace_tbot], stdout=subprocess.PIPE)
 
+# generate dot
 res = subprocess.run(["./dot-logs.py"], stdout=subprocess.PIPE)
 result = str(res.stdout.decode('utf-8'))
 fn = result.split("->")
@@ -28,12 +31,15 @@ fn = fn[1].strip()
 res = subprocess.run(["cp", fn, jenkins_workspace_tbot], stdout=subprocess.PIPE)
 res = subprocess.run(["cp", fn.replace("txt", "jpg"), jenkins_workspace_tbot + "/graph.jpg"], stdout=subprocess.PIPE)
 
+# generate html logs
 res = subprocess.run(["./html-logs.py"], stdout=subprocess.PIPE)
 result = str(res.stdout.decode('utf-8'))
 fn = result.split("->")
 fn = fn[1].strip()
 res = subprocess.run(["cp", fn, jenkins_workspace_tbot], stdout=subprocess.PIPE)
+res = subprocess.run(["cp", "results/html/myscript.js", jenkins_workspace_tbot], stdout=subprocess.PIPE)
 
+# generate statistic
 res = subprocess.run(["./statistic-logs.py"], stdout=subprocess.PIPE)
 result = str(res.stdout.decode('utf-8'))
 fn = result.split("->")
