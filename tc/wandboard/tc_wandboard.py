b3de85b6d56b4ea651836e523c14f8a4d27439be
Heiko Schocher <hs@denx.de>
2019-01-05 17:45:08 +0100

    add tc/wandboard
    Signed-off-by: Heiko Schocher <hs@denx.de>

---
+++ tc/wandboard/tc_wandboard.py
@@ -0,0 +1,152 @@
+import contextlib
+import typing
+import tbot
+from tbot.machine import linux
+from tbot.machine import board
+import os,sys,inspect
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.append(parentdir + '/commonhelper')
+import generic as ge
+
+from tbot.tc import uboot
+
+ub_resfiles = [
+    "System.map",
+    "u-boot.img",
+    "u-boot.bin",
+    "u-boot.cfg",
+    "SPL",
+]
+
+ub_env = [
+    {"name" : "calc_size", "val" : "setexpr fw_sz \${filesize} / 0x200\; setexpr fw_sz \${fw_sz} + 1"},
+    {"name" : "load_spl", "val" : "tftp \${loadaddr} \${spl_file}\;run calc_size"},
+    {"name" : "load_ub", "val" : "tftp \${loadaddr} \${ub_file}\;run calc_size"},
+    {"name" : "upd_prep", "val" : "mmc dev 0"},
+    {"name" : "upd_spl", "val" : "run upd_prep\;mmc write \${loadaddr} 2 \${fw_sz}"},
+    {"name" : "upd_ub", "val" : "run upd_prep\;mmc write \${loadaddr} 8a \${fw_sz}"},
+    {"name" : "cmp_spl", "val" : "run load_spl\;mmc read \${cmp_addr_r} 2 \${fw_sz}\;cmp.b \${loadaddr} \${cmp_addr_r} \${filesize}"},
+    {"name" : "cmp_ub", "val" : "run load_ub\;mmc read \${cmp_addr_r} 8a \${fw_sz}\;cmp.b \${loadaddr} \${cmp_addr_r} \${filesize}"},
+]
+
+@tbot.testcase
+def wandboard_ub_prepare(
+    lab: typing.Optional[linux.LabHost] = None,
+    build: typing.Optional[linux.BuildMachine] = None,
+) -> None:
+    with lab or tbot.acquire_lab() as lh:
+        with build or lh.build() as bh:
+            p = tbot.selectable.UBootMachine.build.ub_patches_path
+            if tbot.selectable.UBootMachine.build.ub_patches_path != None:
+                # copy from host u-boot patches to build host
+                # ToDo get local path
+                lp = f"{tbot.selectable.workdir}/tc/wandboard/patches"
+                # ToDo
+                # get list of files in /home/hs/data/Entwicklung/newtbot/tbot-tbot2go/tc/wandboard/patches
+                # How to run a command on host ?
+                files = ["0001-wandboard-remove-CONFIG_SPL_EXT_SUPPORT-support.patch"]
+                # set correct path to patches on build host
+                tbot.selectable.UBootMachine.build.ub_patches_path = ge.get_path(bh.workdir / "patches/ub-wandboard")
+                tp = tbot.selectable.UBootMachine.build.ub_patches_path
+                print("======== ", tbot.selectable.UBootMachine.build.ub_patches_path)
+                sftp = bh.client.open_sftp()
+                for fn in files:
+                    tbot.log.message(f"put local file {lp}/{fn} to build host {tp}/{fn}")
+                    ret = sftp.put(f"{lp}/{fn}", f"{tp}/{fn}")
+                sftp.close()
+
+@tbot.testcase
+def wandboard_ub_setenv(
+    lh: typing.Optional[linux.LabHost],
+    b: typing.Optional[board.Board],
+    ub: typing.Optional[board.UBootMachine],
+) -> None:
+    # print(" Set Envvars ")
+    ta = lh.tftp_dir
+    f = ta / "SPL"
+    ub.exec0("setenv", "spl_file", ge.get_path(f))
+    f = ta / "u-boot.img"
+    ub.exec0("setenv", "ub_file", ge.get_path(f))
+    ub.exec0("setenv", "serverip", tbot.selectable.LabHost.serverip)
+    ub.exec0("setenv", "netmask", tbot.selectable.LabHost.netmask)
+    ub.exec0("setenv", "ipaddr", tbot.selectable.LabHost.boardip["wandboard"])
+    ub.exec0("setenv", "cmp_addr_r", "11000000")
+    for env in ub_env:
+        ub.env(env["name"], env["val"])
+
+@tbot.testcase
+def wandboard_ub_ins(
+    lh: typing.Optional[linux.LabHost],
+    b: typing.Optional[board.Board],
+    ub: typing.Optional[board.UBootMachine],
+) -> None:
+    # and install
+    ub.exec0("run", "load_spl")
+    ub.exec0("run", "upd_spl")
+    ub.exec0("run", "cmp_spl")
+    ub.exec0("run", "load_ub")
+    ub.exec0("run", "upd_ub")
+    ub.exec0("run", "cmp_ub")
+
+@tbot.testcase
+def wandboard_ub_install(
+    lab: typing.Optional[linux.LabHost] = None,
+    board: typing.Optional[board.Board] = None,
+    ubma: typing.Optional[board.UBootMachine] = None,
+    build: typing.Optional[linux.BuildMachine] = None,
+) -> None:
+    with lab or tbot.acquire_lab() as lh:
+        spl_vers = None
+        ub_vers = None
+        with build or lh.build() as bh:
+            wandboard_ub_prepare(lh, bh)
+            ta = lh.tftp_dir
+            gitp = uboot.build(bh)
+            for f in ub_resfiles:
+                s = gitp / f
+                t = ta / f
+                tbot.tc.shell.copy(s, t)
+                # get SPL / U-Boot Version
+                if "SPL" in f:
+                    # strings /tftpboot/wandboard_dl/tbot/SPL | grep --color=never "U-Boot SPL"
+                    spl_vers = bh.exec0(linux.Raw('strings /tftpboot/wandboard_dl/tbot/SPL | grep --color=never "U-Boot SPL"'))
+                if "u-boot.bin" in f:
+                    # strings /tftpboot/wandboard_dl/tbot/u-boot.bin | grep --color=never "U-Boot 2"
+                    ub_vers = bh.exec0(linux.Raw('strings /tftpboot/wandboard_dl/tbot/u-boot.bin | grep --color=never "U-Boot 2"'))
+
+        with contextlib.ExitStack() as cx:
+            b = cx.enter_context(tbot.acquire_board(lh))
+            ub = cx.enter_context(tbot.acquire_uboot(b))
+            wandboard_ub_setenv(lh, b, ub)
+            wandboard_ub_ins(lh, b, ub)
+
+        # reboot
+        with contextlib.ExitStack() as cx:
+            b = cx.enter_context(tbot.acquire_board(lh))
+            ub = cx.enter_context(tbot.acquire_uboot(b))
+            wandboard_ub_setenv(lh, b, ub)
+            # check new SPL / U-Boot version
+            if spl_vers:
+                if spl_vers not in ub.bootlog:
+                    raise RuntimeError(f"{spl_vers} not found.")
+            if ub_vers:
+                if ub_vers not in ub.bootlog:
+                    raise RuntimeError(f"{ub_vers} not found.")
+
+import ubootpytest
+
+ubt = ubootpytest.Ubootpytest("/home/hs/testframework/hook-scripts", "/work/hs/tbot-workdir/uboot-wandboard")
+
+@tbot.testcase
+def wandboard_ub_call_test_py(
+    lab: typing.Optional[linux.LabHost] = None,
+) -> bool:
+    with lab or tbot.acquire_lab() as lh:
+        try:
+            ubt.ub_call_test_py(lh)
+        except:
+            # fail, but we pass here
+            pass
+
+        return True
