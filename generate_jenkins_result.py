23f0bfb081f5d758dbc6c6ebb34ad7e6cbbacb5d
Heiko Schocher <hs@denx.de>
2019-01-10 09:18:04 +0100

    generate_jenkins_result.py: copy result from test/py
    copy test result from u-boot test/py to jenkins
    results directory
    
    Signed-off-by: Heiko Schocher <hs@denx.de>

--- generate_jenkins_result.py
+++ generate_jenkins_result.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import os
 import subprocess
 import sys
 
@@ -15,6 +16,7 @@ jenkins_workspace = sys.argv[1]
 res = subprocess.run(["./junit-logs.py"], stdout=subprocess.PIPE)
 result = str(res.stdout.decode('utf-8'))
 fn = result.split("->")
+json_fn = fn[0].strip()
 fn = fn[1].strip()
 res = subprocess.run(["cp", fn, jenkins_workspace + "/tbot_results.xml"], stdout=subprocess.PIPE)
 
@@ -25,13 +27,14 @@ with open(jenkins_workspace + "/tbot_results.xml", "r") as f:
         if "classname=" in line:
             line = line.split('"')
             classname = line[1]
+            break
 
 if classname == "":
     print("Could not detect classname")
     sys.exit(-1)
 
 jenkins_workspace_tbot = jenkins_workspace + "/" + classname
-res = subprocess.run(["mkdir", jenkins_workspace_tbot], stdout=subprocess.PIPE)
+res = subprocess.run(["mkdir", "-p", jenkins_workspace_tbot], stdout=subprocess.PIPE)
 
 # generate dot
 res = subprocess.run(["./dot-logs.py"], stdout=subprocess.PIPE)
@@ -65,3 +68,14 @@ fn = fn[0].split("->")
 fn = fn[1].strip()
 res = subprocess.run(["cp", fn, jenkins_workspace_tbot], stdout=subprocess.PIPE)
 res = subprocess.run(["cp", fn.replace("rst", "pdf"), jenkins_workspace_tbot + "/index.pdf"], stdout=subprocess.PIPE)
+
+# collect other result files
+
+# test results from U-Boot test/py
+sname = json_fn.replace("json", "json-testpy-result.html")
+cmd = "sed -i 's/<script src=\"http:\/\/code.jquery.com\/jquery.min.js\"><\/script>/<script src=\"myscript.js\"><\/script>/' " + sname
+os.system(cmd)
+res = subprocess.run(["cp", sname, f"{jenkins_workspace_tbot}/testpy.html"], stdout=subprocess.PIPE)
+
+sname = json_fn.replace("json", "json-multiplexed_log.css")
+res = subprocess.run(["cp", sname, f"{jenkins_workspace_tbot}/multiplexed_log.css"], stdout=subprocess.PIPE)
