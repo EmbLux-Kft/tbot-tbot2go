7f3d111047ee731d85f37d9ac8e345aef0c223ff
Heiko Schocher <hs@denx.de>
2019-01-18 12:51:46 +0100

    labs/smalllaptop.py: add threadripper 16-04 build
    Signed-off-by: Heiko Schocher <hs@denx.de>

--- labs/smalllaptop.py
+++ labs/smalllaptop.py
@@ -1,7 +1,42 @@
+import pathlib
 import typing
 import tbot
 from tbot.machine.linux import lab
 from tbot.machine import linux
+
+class Threadripper1604SSH(linux.SSHMachine, linux.BuildMachine):
+    name = "threadripper-1604-build"
+    username = "hs"
+    hostname = "192.168.1.120"
+    port = 11604
+    dl_dir = "/work/downloads"
+    sstate_dir = f"/work/{username}/tbot2go/yocto-sstate"
+
+    @property
+    def ssh_config(self) -> typing.List[str]:
+        return [f"ProxyJump={self.username}@192.168.1.120"]
+
+    @property
+    def authenticator(self) -> linux.auth.Authenticator:
+        return linux.auth.PrivateKeyAuthenticator(
+            pathlib.PurePosixPath("/home") / "hs" / ".ssh" / "id_rsa"
+    )
+
+    @property
+    def workdir(self) -> "linux.Path[XmglapBuild]":
+        return linux.Workdir.static(self, f"/work/{self.username}/tbot2go")
+
+    @property
+    def toolchains(self) -> typing.Dict[str, linux.build.Toolchain]:
+        return {
+            "generic-armv7a": linux.build.EnvScriptToolchain(
+                linux.Path(
+                    self,
+                    "/opt/eldk/build/work/hws/lweimx6/sdk/environment-setup-armv7a-neon-poky-linux-gnueabi",
+                )
+            ),
+        }
+
 
 class SmalllaptopLab(lab.SSHLabHost, linux.BuildMachine):
     name = "small-lab"
@@ -52,6 +87,11 @@ class SmalllaptopLab(lab.SSHLabHost, linux.BuildMachine):
         }
 
     def build(self) -> linux.BuildMachine:
+        if "threadripper-1604-build" in tbot.flags:
+            return Threadripper1604SSH(self)
         return self
 
 LAB = SmalllaptopLab
+FLAGS = {
+        "threadripper-1604-build":"build on threadripper in ubuntu 16.04 container",
+        }
