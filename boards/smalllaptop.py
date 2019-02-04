71e787e77c474d83c9d4911df6f256b508a9f150
Heiko Schocher <hs@denx.de>
2019-02-04 10:37:43 +0100

    boards/smalllaptop.py: use cleanup function for exiting from kermit
    use cleanup() for exiting from kermit.
    
    Signed-off-by: Heiko Schocher <hs@denx.de>

--- boards/smalllaptop.py
+++ boards/smalllaptop.py
@@ -6,6 +6,7 @@ from tbot.tc import uboot
 
 class SmalllaptopBoard(board.Board):
     connect_wait = 0.0
+    KERMIT_PROMPT = b"C-Kermit>"
 
     def _get_boardname(self):
         if self.name == "wandboard":
@@ -26,7 +27,6 @@ class SmalllaptopBoard(board.Board):
     def connect(self) -> channel.Channel:
         # need here a small sleep, to calm down serial driver
         time.sleep(2)
-        KERMIT_PROMPT = b"C-Kermit>"
         if self.name == 'wandboard':
             n = self._get_boardname()
             cfg_file = f"/home/{tbot.selectable.LabHost.username}/data/Entwicklung/messe/2019/kermrc_{n}"
@@ -46,16 +46,42 @@ class SmalllaptopBoard(board.Board):
         except TimeoutException:
             pass
 
-        if KERMIT_PROMPT in raw:
+        if self.KERMIT_PROMPT in raw:
             raise RuntimeError("Could not get console for ", self.name)
 
         return ch
+
+    def cleanup(self) -> None:
+        # Send  Ctrl-\ (ASCII 28, FS) + C
+        # wait for kermit prompt and send exit
+        if self.channel is not None:
+            self.channel.send("\x1c")
+            self.channel.send("C")
+            # Receive at max the prompt or timeout after 5 seconds
+            raw = b""
+            try:
+                loop = True
+                while loop:
+                    raw += self.channel.recv_n(1, timeout=2.0)
+            except TimeoutError:
+                pass
+            except TimeoutException:
+                pass
+
+            if self.KERMIT_PROMPT not in raw:
+                raise RuntimeError("Could not get kermit console ", self.name)
+            else:
+                self.channel.send("exit")
+
 
     def console_check(self) -> None:
         n = self._get_boardname()
         ret = self.lh.exec0("sispmctl", "-D", "01:01:56:a2:f1", "-g", "3")
         if "off" not in ret:
-            raise RuntimeError("Board is already on, someone might be using it!")
+            ret = self.lh.exec0("sispmctl", "-D", "01:01:56:a2:f1", "-f", "3")
+            time.sleep(2)
+            ret = self.lh.exec0("sispmctl", "-D", "01:01:56:a2:f1", "-o", "3")
+            #raise RuntimeError("Board is already on, someone might be using it!")
 
 
 class SmalllaptopUBootBuildInfo(uboot.BuildInfo):
