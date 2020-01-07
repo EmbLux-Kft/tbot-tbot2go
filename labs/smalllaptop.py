22bb5d14bdc856fd66c1af23d90c11db4da0a577
Heiko Schocher <hs@denx.de>
2020-01-07 11:18:29 +0100

    labs/smalllaptop.py: convert to new tbot
    Signed-off-by: Heiko Schocher <hs@denx.de>

--- labs/smalllaptop.py
+++ labs/smalllaptop.py
@@ -1,55 +1,27 @@
+import functools
+import getpass
 import pathlib
 import typing
 import tbot
-from tbot.machine.linux import lab
-from tbot.machine import linux
+from tbot.machine import connector, linux, board
+import builders
 
-class Threadripper1604SSH(linux.SSHMachine, linux.BuildMachine):
-    name = "threadripper-1604-build"
-    username = "hs"
-    hostname = "192.168.1.120"
-    port = 11604
-    dl_dir = "/work/downloads"
-    sstate_dir = f"/work/{username}/tbot2go/yocto-sstate"
-
-    @property
-    def ssh_config(self) -> typing.List[str]:
-        return [f"ProxyJump={self.username}@192.168.1.120"]
-
-    @property
-    def authenticator(self) -> linux.auth.Authenticator:
-        return linux.auth.PrivateKeyAuthenticator(
-            pathlib.PurePosixPath("/home") / "hs" / ".ssh" / "id_rsa"
-        )
-
-    @property
-    def workdir(self) -> "linux.Path[Threadripper1604SSH]":
-        return linux.Workdir.static(self, f"/work/{self.username}/tbot2go")
-
-    @property
-    def toolchains(self) -> typing.Dict[str, linux.build.Toolchain]:
-        return {
-            "generic-armv7a": linux.build.EnvScriptToolchain(
-                linux.Path(
-                    self,
-                    "/opt/eldk/build/work/hws/lweimx6/sdk/environment-setup-armv7a-neon-poky-linux-gnueabi",
-                )
-            ),
-        }
-
-
-class SmalllaptopLab(lab.SSHLabHost, linux.BuildMachine):
+class SmalllaptopLab(connector.ParamikoConnector, linux.Bash, linux.Lab, linux.Builder):
     name = "small-lab"
-    #hostname = "192.168.1.105"
-    hostname = "192.168.3.10"
+    hostname = "192.168.1.105"
+    #hostname = "192.168.3.10"
     username = "hs"
 
     serverip = "192.168.3.10"
     netmask = "255.255.255.0"
-    boardip = {}
-    boardip["wandboard"] = "192.168.3.20"
     ethaddr = {}
     ethaddr["wandboard"] = "00:1f:7b:b2:00:0f"
+    boardip = {}
+    boardip = {
+        "wandboard": "192.168.3.20",
+    }
+    tftproot = "/var/lib/tftpboot"
+    ub_load_board_env_subdir = "tbot"
 
     @property
     def get_boardname(self) -> str:
@@ -61,15 +33,29 @@ class SmalllaptopLab(lab.SSHLabHost, linux.BuildMachine):
         return tbot.selectable.Board.name
 
     @property
-    def tftp_dir(self) -> "linux.path.Path[SmallLab]":
-        return linux.Path(self, f"/var/lib/tftpboot/{self.get_boardname}/tbot")
+    def nfs_root(self) -> "linux.Path[SmalllaptopLab]":
+        return linux.Path(self, f"/work/tbot2go/nfs")
 
     @property
-    def tftp_dir_board(self) -> "linux.path.Path[SmallLab]":
+    def tftp_root_path(self) -> "linux.Path[SmalllaptopLab]":
+        """
+        returns root tftp path
+        """
+        return linux.Path(self, self.tftproot)
+
+    @property
+    def tftp_dir_board(self) -> "linux.Path[SmalllaptopLab]":
+        """
+        returns tftp path for u-boot tftp command
+        """
         return linux.Path(self, f"{self.get_boardname}/tbot")
 
     @property
-    def workdir(self) -> "linux.path.Path[SmallLab]":
+    def yocto_result_dir(self) -> "linux.Path[SmalllaptopLab]":
+        return linux.Path(self, f"{self.tftproot}/" + tbot.selectable.Board.name + "/tbot/yocto_results")
+
+    @property
+    def workdir(self) -> "linux.Path[SmalllaptopLab]":
         return linux.Workdir.static(self, f"/work/{self.username}/tbot-workdir")
 
     @property
@@ -90,12 +76,25 @@ class SmalllaptopLab(lab.SSHLabHost, linux.BuildMachine):
                 ),
         }
 
-    def build(self) -> linux.BuildMachine:
-        if "threadripper-1604-build" in tbot.flags:
-            return Threadripper1604SSH(self)
-        return self
+    def build(self) -> linux.Builder:
+        if "pollux-build" in tbot.flags:
+            return builders.PolluxSSH(self)
+        elif "xpert-build" in tbot.flags:
+            return builders.XpertSSH(self)
+        elif "hercules-build" in tbot.flags:
+            return builders.HerculesSSH(self)
+        elif "hercules-1604-build" in tbot.flags:
+            return builders.Hercules1604SSH(self)
+        elif "smalllaptop-build" in tbot.flags:
+            return self
+        elif "threadripper-build" in tbot.flags:
+            return builders.ThreadripperSSH(self)
+        elif "threadripper-1604-build" in tbot.flags:
+            return builders.Threadripper1604SSH(self)
+        elif "threadripper-1604-kas-build" in tbot.flags:
+            return builders.Threadripper1604kasSSH(self)
+        elif "xmg-build" in tbot.flags:
+            return builders.xmgSSH(self)
+        raise RuntimeError ("build Machine not specified")
 
 LAB = SmalllaptopLab
-FLAGS = {
-        "threadripper-1604-build":"build on threadripper in ubuntu 16.04 container",
-        }
