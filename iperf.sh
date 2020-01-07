c6cf477c2478d417dea4272f231e20ba004f9e3f
Heiko Schocher <hs@denx.de>
2020-01-07 18:05:58 +0100

    iperf.sh: do not copy to 192.168.1.106
    Signed-off-by: Heiko Schocher <hs@denx.de>

--- iperf.sh
+++ iperf.sh
@@ -3,5 +3,5 @@
 if [ -f "results/iperf/iperf.dat" ]
 then
     gnuplot -e 'output_file="results/iperf/iperf.jpg";input_file="results/iperf/iperf.dat";graph_title="iperf"' results/iperf/balkenplot_iperf.sem
-    scp results/iperf/iperf.jpg hs@192.168.1.106:/home/hs/data/Entwicklung/bbb/src
+    #scp results/iperf/iperf.jpg hs@192.168.1.106:/home/hs/data/Entwicklung/bbb/src
 fi
