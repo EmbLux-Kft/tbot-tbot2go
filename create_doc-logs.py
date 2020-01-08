34018ca6df8745b2de05a14dbcbf4ed95b4e1eee
Heiko Schocher <hs@denx.de>
2020-01-08 09:32:04 +0100

    create_doc-logs.py: convert to new tbot
    Signed-off-by: Heiko Schocher <hs@denx.de>

--- create_doc-logs.py
+++ create_doc-logs.py
@@ -6,7 +6,7 @@ import os
 import sys, getopt
 
 CONFS = [
-    ("smalllaptop", "wandboard", "./generators/create_doc_test"),
+    ("smalllaptop", "wandboard", "./generators/iperf_test"),
 ]
 
 def do_conf(cfg: typing.Tuple[str, str, str], tbotpath) -> None:
