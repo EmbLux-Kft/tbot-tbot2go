922f16c45979a9db209e9a8f68fe655482647aa7
Heiko Schocher <hs@denx.de>
2020-01-10 08:10:48 +0100

    add clear_all_logs_and_results.sh
    clear all logs and results.
    
    Signed-off-by: Heiko Schocher <hs@denx.de>

---
+++ clear_all_logs_and_results.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+rm -rf log/*
+rm -rf results/doc/*
+rm -rf results/dot/*
+rm -rf results/html/smalllaptop*
+rm -rf results/junit/*
+rm -rf results/latency/smalllaptop*
+rm -rf results/ptest/smalllaptop*
+rm -rf results/stats/smalllaptop*
